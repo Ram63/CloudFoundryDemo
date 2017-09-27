683cd7f7d624b75d17f8052aeb2869fcc1b447ab
Stephan Michard <stephan.michard@gmail.com>
2017-09-27 23:34:28 +0200

    testing pipeline

--- set-pipeline.sh
+++ set-pipeline.sh
@@ -2,7 +2,7 @@
 
 if [ -z "$1" ]
   then
-    fly -t lite set-pipeline -p cloud_foundry_demo -c ci/pipeline2.yml -l ci/credentials.yml
+    fly -t lite set-pipeline -p cloud_foundry_demo -c ci/pipeline.yml -l ci/credentials.yml
   else
-    fly -t $1 set-pipeline -p cloud_foundry_demo -c ci/pipeline2.yml -l ci/credentials.yml
+    fly -t $1 set-pipeline -p cloud_foundry_demo -c ci/pipeline.yml -l ci/credentials.yml
 fi
